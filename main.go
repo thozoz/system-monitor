3a7b653101ffa41cae0b5d812f7706e16156838a
Efe Özan <efeozan@marun.edu.tr>
2026-03-10 09:15:08 +0300

    feat: add RAM usage to /api/status

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// RAM (Virtual Memory) bilgilerini okuyoruz
+	// v değişkeni pointer, err ise hata kontrolü için
+	v, err := mem.VirtualMemory()
 
-	fmt.Fprintf(w, "r degiskeninin bellekteki adresi: %p\n\n", r)
-	fmt.Fprintf(w, "adres icindeki veri:\n%+v", r)
+	if err != nil {
+		fmt.Fprintf(w, "RAM bilgisi okunamadi: %v", err)
+		return
+	}
+
+	// Bilgiler byte cinsinden, GB'a çevirmek için 1024'e arka arkaya 3 kere bölüyoruz.(byte->kilobayt->megabayt->gigabyte)
+	toplamGB := v.Total / 1024 / 1024 / 1024
+	kullanilanGB := v.Used / 1024 / 1024 / 1024
+
+	// Tarayıcıya düz metin olarak bilgileri yolla
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "--- SISTEM RAM DURUMU ---\n")
+	fmt.Fprintf(w, "Toplam RAM: %v GB\n", toplamGB)
+	fmt.Fprintf(w, "Kullanilan RAM: %v GB\n", kullanilanGB)
+
+	fmt.Fprintf(w, "Kullanim Orani: %%%.2f\n", v.UsedPercent)
 }
 
 func main() {
@@ -19,6 +37,6 @@ func main() {
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Sunucu baslatilamadi:", err)
+		fmt.Println("HATA:", err)
 	}
 }
